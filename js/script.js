1938fad83cee012f5755adee9bd269f7d913f45c
Tasnim Mohona <67105895+tasnimmohona62@users.noreply.github.com>
2020-12-28 19:03:24 +0600

    popup

--- js/script.js
+++ js/script.js
@@ -18,27 +18,17 @@ $('.counter').counterUp({
 });
 });
 
+//popup
+$(document).ready(function(){
+    setTimeout(function(){
+      $("#popUpmain").css('display','block');
+    }, 2000);
+});
 
-    var acwrapper = document.getElementById('ac-wrapper');
-    var yes = document.getElementById("yes");
-    var no = document.getElementById("no");
-    function PopUp(){
-      yes.addEventListener("click",function(){
-        yes.style.display=" https://tasnimmohona.github.io/Educational_Institute/"
-       //  var d = new Date();
-       //  var s = document.getElementById(+d.getDay());
-       //   acwrapper.style.display = (acwrapper.style.display == 'block') ? 'none' : 'block';
-      });
-   
-          
-              
-      no.addEventListener("click",function(){
-        alert("Sorryy! you are not eligible");
-   
-     });
-
-    }
-            
+$("#subscribeId").click(function(){
+  $("#popUpmain").css('display','none');
+});
+        
 
   
 
